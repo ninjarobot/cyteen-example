d4a171c6884644c4d1559852f0f2c9be24772162
Dave Curylo <curylod@asme.org>
2017-10-19 13:37:13 -0400

    Initial commit.

---
+++ Program.fs
@@ -0,0 +1,26 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+let squareRoot x =
+    let isCloseEnough guess = 
+        let acceptableVariance = x / 10000.0
+        let guessSquared = guess * guess
+        x - acceptableVariance < guessSquared &&
+        x + acceptableVariance > guessSquared 
+    
+    let nextGuess y =
+        (y + (x/y)) / 2.0
+
+    let rec guessFn lastGuess =
+        let closeEnough = isCloseEnough lastGuess
+        match closeEnough with
+        | true -> lastGuess
+        | false -> lastGuess |> nextGuess |> guessFn 
+    
+    guessFn (x / 2.0)
+
+[<EntryPoint>]
+let main argv =
+    squareRoot 100.0 |> printfn "%f"
+    0 // return an integer exit code
